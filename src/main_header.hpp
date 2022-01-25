abd0f51f378be9dee51d9c3979e9f1de4ff634c8
Vincent Atmadja <vincentpatmadja@gmail.com>
2022-01-25 22:23:54 +0700

    Create header file

---
+++ src/main_header.hpp
@@ -0,0 +1,46 @@
+#ifndef MAINHEADER_H
+#define MAINHEADER_H
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+
+//Struct for each word
+struct word{
+    string content;
+    int length;
+    bool done;
+};
+
+// Some global variable used
+vector<vector<char>>board;
+vector<word>problem;
+
+int row, column, probnumb;
+
+
+//Function declaration
+//Function for checking board;
+
+void checkHorizontal(vector<vector<char>>board, vector<word>allw, int col, int row, int lword);
+void checkVertikal(vector<vector<char>>board, vector<word>allw, int col, int row, int lword);
+void checkDiagonalL(vector<vector<char>>board, vector<word>allw, int col, int row, int lword);
+void checkDiagonalR(vector<vector<char>>board, vector<word>allw, int col, int row, int lword);
+
+//Function for print solution
+void printHorizontal(int row, int col, int length, int sizer, int sizecol, vector<vector<char>>board);
+void printVertikal(int row, int col, int length, int sizer, int sizecol, vector<vector<char>>board);
+void printDiagonalL(int row, int col, int sizer, int sizecol, vector<vector<char>>board);
+void printDiagonalR(int row, int col, int sizer, int sizecol, vector<vector<char>>board);
+
+
+
+
+
+
+
+
+
+#endif
