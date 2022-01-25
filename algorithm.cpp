80f966c69f44d6cebda6a194c8141b24e1a88226
Vincent Atmadja <vincentpatmadja@gmail.com>
2022-01-25 12:18:08 +0700

    first horizontal check, for checking left to right

--- algorithm.cpp
+++ algorithm.cpp
@@ -8,8 +8,19 @@ struct word{
 };
 
 
-void printHorizontal(){
+void printHorizontal(int row, int col, int length, int sizer, int sizecol, vector<vector<char>>board){
+    for(int i = 0; i < sizer; i ++){
+        for(int j = 0; j < sizecol; j ++){
+            if(i == row && j >= col && j < col + length){
+                cout << board[i][j];
+            }
+            else{
+                cout << "-";
+            }
 
+            cout << " \n"[j == sizecol-1];
+        }
+    }
 }
 
 void printVertikal(){
@@ -19,11 +30,36 @@ void printVertikal(){
 void printDiagonal(char* board, vector<pair<string,int>>word){
 
 }
+/*
+    a b c d 4  
+*/
 
-void checkHorizontal(vector<vector<char>>board, vector<pair<string,int>>word, int col, int row){
-    for(int i = 0; i < col; i ++){
-        for(int j = 0; j < row; j ++ ){
-            
+
+void checkHorizontal(vector<vector<char>>board, vector<word>allw, int col, int row, int lword){
+    for(int i = 0; i < row; i ++){
+        for(int j = 0; j < col; j ++ ){
+            int ind = 0;
+            for(int k = 0; k < lword; k ++){
+                if(allw[k].length > col - j){
+                    break;
+                }
+
+                if(allw[k].cntnt[ind] == board[i][j] && !allw[k].done){
+                    bool same = true;
+                    for(int l = j + 1; l < j + allw[k].length; l ++){
+                        ind ++;
+                        if(allw[k].cntnt[ind] != board[i][l]){
+                            same = false;
+                            break;
+                        }
+                    }
+
+                    if(same){
+                        allw[k].done = true;
+                        printHorizontal(i, j, allw[k].length, row, col, board);
+                    }
+                }
+            }
         }
     }
 }
@@ -33,7 +69,7 @@ void checkHorizontal(vector<vector<char>>board, vector<pair<string,int>>word, in
 
 int main(){
     int n, m, k;
-    cin >> n >> m;
+    cin >> n >> m >> k;
     vector<vector<char>>board;
     vector<word>allw;
 
@@ -60,7 +96,7 @@ int main(){
         return a.length < b.length;
     });
     
-
+    checkHorizontal(board, allw, m, n, allw.size());
 
 
 
